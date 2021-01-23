00adfc29cf5f6b5a8601de13132c44e4bc54b32b
Polarified <polarified@gmail.com>
2021-01-23 19:05:45 +0200

    level11

--- ctfs/python_challenge/level11.py
+++ ctfs/python_challenge/level11.py
@@ -1,12 +1,22 @@
 from PIL import Image
-from pprint import pprint
+import pprint
 
-img = Image.open('cave.jpg')
+url = 'http://www.pythonchallenge.com/pc/return/5808.html'
+
+img = Image.open('static/cave.jpg')
 width, height = img.size
-pixels = img.load()
+pixels = list(img.getdata())
 
-pixlist = []
-for x in range(width):
-    row = [pixels[x, y] for y in range(height)]
-    pixlist.append(row)
-    print(row)
+pprint.pprint(pixels[:100])
+
+even = [pixels[i] for i in range(0, len(pixels), 2)]
+odd = [pixels[i] for i in range(1, len(pixels), 2)]
+
+
+e = Image.new(img.mode, img.size)
+e.putdata(even)
+e.show()
+
+o = Image.new(img.mode, img.size)
+o.putdata(odd)
+o.show()
