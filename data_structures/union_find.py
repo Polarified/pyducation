829fb077a4cafdcfb0904ecca8bd8968498a4850
Polarified <polarified@gmail.com>
2022-02-06 17:24:13 +0200

    no clue

---
+++ data_structures/union_find.py
@@ -0,0 +1,15 @@
+"""
+Create a mapping between element and index. This works with an array.
+To find which component a certain element belongs to, we travel up to the root until we find a self loop (an element who's parent is itself).
+To unify two elements we find the two root nodes of the groups each element belongs to, then we make one of the root nodes be the parent of the other if they are different.
+"""
+
+
+class UnionFind(list):
+
+    def find(self, n):
+        i = self.index(n)
+        while (self.index(n) != n)
+
+    def union(self, n, m):
+
