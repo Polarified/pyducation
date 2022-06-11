542c40598e7eebaf1c2b6c11974e02e8ed07594b
Polarified <polarified@gmail.com>
2022-06-11 22:11:45 +0300

    random

--- ctfs/python_challenge/level11.py
+++ ctfs/python_challenge/level11.py
@@ -1,5 +1,4 @@
 from PIL import Image
-import pprint
 
 url = 'http://www.pythonchallenge.com/pc/return/5808.html'
 
@@ -7,16 +6,10 @@ img = Image.open('static/cave.jpg')
 width, height = img.size
 pixels = list(img.getdata())
 
-pprint.pprint(pixels[:100])
-
 even = [pixels[i] for i in range(0, len(pixels), 2)]
 odd = [pixels[i] for i in range(1, len(pixels), 2)]
 
 
-e = Image.new(img.mode, img.size)
-e.putdata(even)
-e.show()
-
 o = Image.new(img.mode, img.size)
-o.putdata(odd)
+o.putdata(odd + even)
 o.show()
--- ctfs/python_challenge/level12.py
+++ ctfs/python_challenge/level12.py
@@ -1,10 +1,28 @@
-import pprint
 from PIL import Image
+import numpy as np
+import pprint
 
 url = 'http://www.pythonchallenge.com/pc/return/evil.html'
 
 img = Image.open('static/evil1.jpg')
 width, height = img.size
-pixels = img.load()
+pixels = list(img.getdata())
 
-pprint.pprint([pixels[x, y] for x in range(width) for y in range(height)])
+rows = [pixels[x:width + x] for x in range(0, len(pixels), width)]
+
+print(len(rows), len(rows[0]))
+
+greens = [rows[x: x + 2] for x in range(1, len(rows), 6)]
+blues = [rows[x: x + 2] for x in range(2, len(rows), 6)]
+reds = [rows[x: x + 2] for x in range(0, len(rows), 6)]
+
+print(len(greens), len(greens[0]), len(greens[0][0]))
+
+fr = [item for sublist in reds for item in sublist]
+fg = [item for sublist in greens for item in sublist]
+fb = [item for sublist in blues for item in sublist]
+
+e = Image.new(img.mode, img.size)
+e.putdata(fr + fg + fb)
+e.show()
+e.save('static/triple.jpg')
